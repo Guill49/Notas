ff651ece6e0125ad74aacc7899af998bdaa54210
Guillermo <guillermodlz-49@outlook.com>
2024-07-15 13:47:50 -0600

    mejors

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const path = require('path')
 
 const app = express();
 const PORT = 3000;
+const PUBLIC =path.join(__dirname, 'public');
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(PUBLIC));
 
 app.get('/', (req,res) => {
     console.log('loading home..');
-    res.send('hola mundo');
+    res.sendFile(path.join(PUBLIC, 'index.html'));
 });
 
 app.listen(PORT, () => {
