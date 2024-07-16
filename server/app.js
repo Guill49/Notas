bb84b8d7f2abce9101775f683c5a7a2684cec54c
Guillermo <guillermodlz-49@outlook.com>
2024-07-15 22:23:54 -0600

    agregando cosas, pero embotado y perdido

--- server/app.js
+++ server/app.js
@@ -18,6 +18,16 @@ app.get('/', (req, res) =>{
 });
 
 
+app.post('/create-Note', (req, res) =>{
+  console.info('Create has been called..\n');
+  const id = req.body.id;
+  const title = req.body.title;
+  const content = req.body.content;
+  const tags = req.body.tags;
+
+  res.send('data received');
+})
+
 app.use((req, res, next) => {
   res.status(404).send('pagina no encontrada');
 })
